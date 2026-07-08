f656cb6617d72ddcf72230905beb82f85fcd85f5
IrfanSoftwareEngineer <irfanullah2220@gmail.com>
2026-07-08 23:55:28 +0530

    adding data

--- src/url/baseUrl.js
+++ src/url/baseUrl.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const api = axios.create({
   
-    baseURL: 'https://mcbackend.vercel.app',
+    baseURL: 'https://arg-backend.vercel.app/',
     headers: {
       'Content-Type': 'application/json',
     },
